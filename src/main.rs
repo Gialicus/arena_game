8ec56f45ddecfc0824ff9ca1a61e536bce7d6bb3
giali.coletta <51245135+Gialicus@users.noreply.github.com>
2024-11-13 23:13:53 +0100

    feat(arena): shuffle players

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ async fn main() {
     let archer = Character::new("Arciere", skills_player2);
     let rogue = Character::new("Ladro", skills_player3);
     let mage = Character::new("Mago", skills_player4);
+    println!("\n*** START TOURNAMENT ***\n");
     let winner = start_tournament(vec![war, archer, rogue, mage]).await;
     println!("\n*** WINNER ***\n{}\n", winner)
 }
